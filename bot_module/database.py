a75fd8e810a1eea3c41dd2aab472c713cff4d9c6
hyunmila <kamilaak90801@gmail.com>
2023-07-10 17:00:31 +0200

    database

--- bot_module/database.py
+++ bot_module/database.py
@@ -1,11 +1,12 @@
+import os
 import shelve
 import time
 import numpy as np
 import shutil
 import json
-from bot_module.chatbot import chatbot
+from chatbot import chatbot
 
-from bot_module.training import start_training
+from training import start_training
 
 class User:
     def __init__(self, username):
@@ -82,12 +83,101 @@ class User:
     
 
 
+class Database:
+    def __init__(self):
+        # self.u=User('user')
+        self.usernames=shelve.open("bot_module/database/userdata/usernames")
+        self.userdata=shelve.open("bot_module/database/userdata/userdata")
+        self.userdb=shelve.open("bot_module/database/userdata/userdb")
+        self.db_list=os.listdir(path='bot_module/database')
+        self.db_list.remove('userdata')
+        self.keys_list=[list(self.usernames.keys()), list(self.userdata.keys()),
+                        list(self.userdb.keys()), self.db_list]
+
+    
+    def list_len(self):
+        lengths=[len(self.keys_list[0]), len(self.keys_list[1]), 
+                    len(self.keys_list[2]), len(self.keys_list[3])]
+        return lengths
+    
+    def del_from_list(self,id,lengths):
+        if id==3:
+            for elem in self.keys_list[id]:
+                if ((elem not in self.keys_list[0]) or
+                    (elem not in self.keys_list[1]) or
+                    (elem not in self.keys_list[2])):
+                    print("Deleting ",elem," from database")
+                    try:
+                        shutil.rmtree('bot_module/database/'+str(elem))
+                    except NotADirectoryError:
+                        os.remove('bot_module/database/'+str(elem))
+                    # lengths=self.list_len()
+                    self.db_list.remove(elem)
+                    print(self.db_list)
+                    lengths[id]=len(self.db_list)
+        else:
+            list_todel=self.keys_list[id]
+            # temp=self.keys_list
+            # print(self.keys_list)
+            temp=self.keys_list.copy()
+            # print(temp)
+            temp.pop(id)
+            # print(temp)
+            # print(self.keys_list)
+            userx_list=[self.usernames,self.userdata,self.userdb]
+            for elem in list_todel:
+                if ((elem not in temp[0]) or
+                    (elem not in temp[1]) or
+                    (elem not in temp[2])):
+                    print("Deleting ",elem,":",userx_list[id][elem])
+                    # print (self.userx_list[id][elem])
+                    del (userx_list[id][elem])
+                    # lengths=self.list_len()
+                    lengths[id]=len(list((userx_list[id]).keys()))
+                    print(list((userx_list[id]).keys()))
+                    # self.keys_list[id]=list(userx_list[id].keys)
+                    # userx_list[id].close()
+                    # print(userx_list[id][elem])
+                    # print(lengths)
+                    # print(self.keys_list)
+        print(lengths)
+        return lengths
+
+
+
+    def is_equal(self):
+        self.usernames['ababa']='nanana'
+        self.userdata['ababa']=100
+        lengths=self.list_len()
+        print(lengths)
+
+        
+        print("before\n")
+        for i in range(len(self.keys_list)):
+            print(self.keys_list[i])
+        
+        is_not=(lengths[0]!=lengths[1] or lengths[0]!=lengths[2] or lengths[0]!=lengths[3]) # True if not equal
+        while is_not:
+            yn=input("Database error, continue? [y/n]: ")
+            if yn.lower()=='n':break
+            count=lengths.count(max(lengths))
+            id=lengths.index(max(lengths))
+            lengths=self.del_from_list(id,lengths)
+            count=lengths.count(max(lengths))
+            if count==4:
+                is_not=False
+
+
+
+
+#'ababa', (27648, 21)
+
 
 
 def main():
     # 1 for debug, 0 for user
     mode = 0
-
+    Database().is_equal()
 
     username_in = input("Enter your username or 's' to skip: ")
     if username_in.lower()=='s':
